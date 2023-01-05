cddba0917248a8969db272a880af53a40b8f71fe
Siarhei Rudnitski <siarhei.rudnitskiy@gmail.com>
2023-01-05 13:45:23 +0300

    Initial commit

--- shahmat.cpp
+++ shahmat.cpp
@@ -1,31 +1,45 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 int main() 
 {
-	int a = 0;
-	int b = 0;
-	int c = 0;
-	int d = 0;
+	int x1 = 0;
+	int y1 = 0;
+	int x2 = 0;
+	int y2 = 0;
+	int fx = 0;
+	int fy = 0;
 	
-	cout << "Enter coordinates of the first rook number1: ";
-	cin >> a; 
-	cout << "Enter coordinates of the first rook number2: ";
-	cin >> b; 	
-	cout << "Enter second rook coordinates number1: ";
-	cin >> c; 	
-	cout << "Enter second rook coordinates number2: ";
-	cin >> d; 
+	cout << "Enter the horizontal coordinates of the first horse: ";
+	cin >> x1; 
+	cout << "Enter the vertical coordinates of the first horse: ";
+	cin >> y1; 	
+	cout << "Enter the horizontal coordinates of the second horse: ";
+	cin >> x2; 	
+	cout << "Enter the vertical coordinates of the second horse: ";
+	cin >> y2; 
 	
-	if (a == c and b != d)
-		cout << "Rooks hit each other";
-	else if (a == c and b == d)
-		cout << "Input Error";
-	else if (a <= 0 or b <= 0 or c <= 0 or d <=0 or a > 8 or b > 8 or c > 8 or d > 8)
-		cout << "Input Error";
+	if (x1 == x2 and y1 == y2)
+	{
+		cout << "Input Error" << endl;
+	}
+	else if (x1 <= 0 or x2 <= 0 or y1 <= 0 or y2 <=0 or x1 > 8 or x2 > 8 or y1 > 8 or y2 > 8)
+	{
+		cout << "Input Error" << endl;
+	}
 	else
-		cout << "Rooks do not attack each other";
+	{
+		fx = abs(x1 - x2);
+		fy = abs(y1 - y2);
+	
+		if (fx == 2 and fy == 1 or fx == 1 and fy == 2)
+			cout << "Horses hit each other";
+		else 
+			cout << "Horses do not attack each other";	
+	}
 		
 	return 0;
 }
+
